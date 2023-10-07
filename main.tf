a683013e6d94f281fe1fecdad9658179ccbf93ff
Rodrigo Pillar Vianna <rpvianna2009@gmail.com>
2023-10-07 18:18:57 -0300

    Atualizados arquivos tf

--- main.tf
+++ main.tf
@@ -11,15 +11,107 @@ provider "aws" {
   region = var.region
 }
 
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+resource "aws_vpc" "fiap" {
+  cidr_block           = var.vpc_cidr_block
+  enable_dns_hostnames = true
+  tags = {
+    Name = "FIAP"
+  }
+}
+
+resource "aws_internet_gateway" "fiap" {
+  vpc_id = aws_vpc.fiap.id
+
+  tags = {
+    Name = "FIAP"
+  }
+}
+
+resource "aws_subnet" "fiap_public" {
+  count                   = var.subnet_count.public
+  vpc_id                  = aws_vpc.fiap.id
+  cidr_block              = var.public_subnet_cidr_blocks[count.index]
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "FIAP-Public-${count.index}"
+  }
+}
+
+resource "aws_subnet" "fiap_private" {
+  count                   = var.subnet_count.private
+  vpc_id                  = aws_vpc.fiap.id
+  cidr_block              = var.private_subnet_cidr_blocks[count.index]
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = false
+  tags = {
+    Name = "FIAP-Private-${count.index}"
+  }
+}
+
+resource "aws_route_table" "fiap_public" {
+  vpc_id = aws_vpc.fiap.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.fiap.id
+  }
+}
+
+resource "aws_route_table_association" "fiap_public" {
+  count          = var.subnet_count.public
+  subnet_id      = aws_subnet.fiap_public[count.index].id
+  route_table_id = aws_route_table.fiap_public.id
+}
+
+resource "aws_route_table" "fiap_private" {
+  vpc_id = aws_vpc.fiap.id
+}
+
+resource "aws_route_table_association" "fiap_private" {
+  count          = var.subnet_count.private
+  subnet_id      = aws_subnet.fiap_private[count.index].id
+  route_table_id = aws_route_table.fiap_private.id
+}
+
+resource "aws_security_group" "fiap_rds" {
+  name   = "FIAP-RDS"
+  vpc_id = aws_vpc.fiap.id
+
+  ingress {
+    description = "MySQL traffic"
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "FIAP"
+  }
+}
+
+resource "aws_db_subnet_group" "fiap" {
+  name       = "fiap"
+  subnet_ids = [for subnet in aws_subnet.fiap_private : subnet.id]
+}
+
 resource "aws_db_instance" "fiap_db" {
-  allocated_storage   = 20
-  db_name             = "fiap_db"
-  engine              = "mysql"
-  engine_version      = "8.0.33"
-  instance_class      = "db.t2.micro"
-  username            = "admin"
-  password            = "fiap1234"
-  skip_final_snapshot = true
-  multi_az            = false
-  identifier          = "fiap-db"
+  allocated_storage      = var.settings.database.allocated_storage
+  db_name                = var.settings.database.db_name
+  engine                 = var.settings.database.engine
+  engine_version         = var.settings.database.engine_version
+  instance_class         = var.settings.database.instance_class
+  username               = var.db_username
+  password               = var.db_password
+  skip_final_snapshot    = var.settings.database.skip_final_snapshot
+  publicly_accessible    = var.settings.database.publicly_accessible
+  multi_az               = var.settings.database.multi_az
+  identifier             = var.settings.database.identifier
+  db_subnet_group_name   = aws_db_subnet_group.fiap.name
+  vpc_security_group_ids = [aws_security_group.fiap_rds.id]
 }
--- variables.tf
+++ variables.tf
@@ -3,3 +3,68 @@ variable "region" {
   type        = string
   default     = "us-east-1"
 }
+
+variable "vpc_cidr_block" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet_count" {
+  description = "Number of subnets to create"
+  type        = map(number)
+  default = {
+    public  = 1
+    private = 2
+  }
+}
+
+variable "settings" {
+  description = "Settings for the RDS"
+  type        = map(any)
+  default = {
+    "database" = {
+      "allocated_storage"   = 20
+      "db_name"             = "fiap_db"
+      "engine"              = "mysql"
+      "engine_version"      = "8.0.33"
+      "instance_class"      = "db.t2.micro"
+      "skip_final_snapshot" = true
+      "publicly_accessible" = true
+      "multi_az"            = false
+      "identifier"          = "fiap-db"
+    }
+  }
+}
+
+variable "public_subnet_cidr_blocks" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default = [
+    "10.0.1.0/24",
+    "10.0.2.0/24",
+    "10.0.3.0/24",
+  ]
+}
+
+variable "private_subnet_cidr_blocks" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+  default = [
+    "10.0.101.0/24",
+    "10.0.102.0/24",
+    "10.0.103.0/24",
+  ]
+}
+
+variable "db_username" {
+  description = "Username for the database"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_password" {
+  description = "Password for the database"
+  type        = string
+  sensitive   = true
+}
